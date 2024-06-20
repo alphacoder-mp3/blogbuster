3fb7f610f7b6c5adba07d1aaca63220d9c264f1e
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-06-20 20:05:28 +0530

    Minor updates

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,7 +2,19 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest) {
   const token = req.cookies.get('token'); // Retrieve the token from the cookies
+  const authRoutes = ['/', '/dashboard', '/profile', '/settings'];
+  console.log({ req });
+  console.log('Request URL:', req.nextUrl.pathname); // Log the request URL
+  console.log('Token:', token);
+  if (authRoutes.some(route => req.nextUrl.pathname.startsWith(route))) {
+    if (!token) {
+      return NextResponse.redirect(new URL('/signin', req.url)); // Redirect to /signin if no token is present
+    }
+  }
 
+  if (req.nextUrl.pathname.startsWith('/')) {
+    return NextResponse.rewrite(new URL('/signin', req.url));
+  }
   if (token) {
     req.headers.set('Authorization', `${token}`);
   }
@@ -16,5 +28,5 @@ export async function middleware(req: NextRequest) {
 
 // Specify the routes where you want the middleware to run
 export const config = {
-  matcher: ['/api/v1/blog'],
+  matcher: ['/api/v1/blog', '/', '/*', '/:path*'],
 };
